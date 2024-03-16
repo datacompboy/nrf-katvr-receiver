0f3d78d19711189ecc5dadef604dc4a9e6953ccc
Anton Fedorov <datacompboy@gmail.com>
2024-03-16 21:38:04 +0100

    Create restore-sensor-config.ps1
    Add script that restores sensor configuration after re-flashing.

---
+++ scripts/restore-sensor-config.ps1
@@ -0,0 +1,73 @@
+# Script configuration
+$katPath = "C:\Program Files (x86)\KAT Gateway\"
+
+# Load Gateway's communication helper library
+if (!(Test-Path $katPath -PathType Container)) {
+    throw  "KAT Gateway expected to be installed in '${katPath}'"
+}
+
+if (Get-Process -name "KAT Gateway" -ErrorAction SilentlyContinue) {
+    throw "Please stop KAT Gateway."
+}
+
+Add-Type -Path "C:\Program Files (x86)\KAT Gateway\IBizLibrary.dll"
+
+#
+# Detect known Receiver version
+#
+[IBizLibrary.ComUtility]::KatDevice='walk_c2'
+if (Test-Path([IBizLibrary.ComUtility]::Device_Config_File_Walk_C2_Path)) {
+    Write-Host "Gateway with configuration for Kat Walk C2/C2+ detected."
+} else {
+    [IBizLibrary.ComUtility]::KatDevice='walk_c2_core'
+    if (Test-Path([IBizLibrary.ComUtility]::Device_Config_File_Walk_C2_Path)) {
+        Write-Host "Gateway with configuration for Kat Walk C2 Core detected."
+    } else {
+        throw "Not found gateway configuration for neither Kat Walk C2/C2+ nor C2 Core."
+    }
+}
+[IBizLibrary.Configs].GetMethod('C2ReceiverPairingInfoRead').invoke($null, $null)
+if ($null -eq [IBizLibrary.KATSDKInterfaceHelper]::receiverPairingInfoSave.ReceiverSN) {
+    throw "Gateway configuration for the treadmill is not found."
+}
+
+[IBizLibrary.KATSDKInterfaceHelper].GetMethod('GetDeviceConnectionStatus').invoke($null, $null)
+$dev = New-Object IBizLibrary.KATSDKInterfaceHelper+KATModels
+for($devNo=0; $devNo -lt [IBizLibrary.KATSDKInterfaceHelper]::deviceCount; $devNo ++) {
+    [IBizLibrary.KATSDKInterfaceHelper]::GetDevicesDesc([ref]$dev, $devNo)
+    if ($dev.deviceType -eq 0 -and $dev.device -match "walk c2 foot") {
+        break;
+    }
+}
+
+if (-not($dev.device -match "walk c2 foot")) {
+    throw "Please connect foot sensor."
+}
+
+$id = -1
+[IBizLibrary.KATSDKInterfaceHelper]::ReadDeviceId($dev.serialNumber, [ref]$id)
+if ($id -eq 2) {
+    Write-Warning "Sensor is already configured to be left sensor"
+}
+elseif ($id -eq 3) {
+    Write-Warning "Sensor is already configured to be right sensor"
+}
+elseif ($id -ne 0) {
+    throw "Sensor configuration error!"
+}
+
+$sensor = New-Object IBizLibrary.KATSDKInterfaceHelper+sensorInformation
+[IBizLibrary.KATSDKInterfaceHelper]::GetSensorInformation([ref]$sensor, $dev.serialNumber)
+$leftmac = [IBizLibrary.KATSDKInterfaceHelper]::receiverPairingInfoSave.ReceiverPairingByte[7..12]
+$rightmac = [IBizLibrary.KATSDKInterfaceHelper]::receiverPairingInfoSave.ReceiverPairingByte[13..19]
+if(-not(Compare-Object $leftmac $sensor.mac)) {
+    [IBizLibrary.KATSDKInterfaceHelper]::WriteDeviceId($dev.serialNumber, 2)
+    Write-Host "Made the sensor to be Left Foot"
+}
+elseif(-not(Compare-Object $rightmac $sensor.mac)) {
+    [IBizLibrary.KATSDKInterfaceHelper]::WriteDeviceId($dev.serialNumber, 3)
+    Write-Host "Made the sensor to be Right Foot"
+}
+else {
+    throw "The sensor's mac is not paired to the treadmill"
+}
