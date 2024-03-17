204e905da29787713b0dc94eabca0f08c8810e14
Anton Fedorov <datacompboy@gmail.com>
2024-03-17 16:52:38 +0100

    Update update-feet-firmware.cmd

--- scripts/update-feet-firmware.cmd
+++ scripts/update-feet-firmware.cmd
@@ -1,2 +1,2 @@
-powershell.exe -ExecutionPolicy Bypass -File flush-feet-sensor.ps1 --patch:.\foot-patch.ps1
+powershell.exe -ExecutionPolicy Bypass -File flush-feet-sensor.ps1 --patch .\foot-patch.ps1
 pause
